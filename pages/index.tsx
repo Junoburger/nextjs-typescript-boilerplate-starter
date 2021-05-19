7c33c4f4f083db056a228276a0dda835df9ce449
Junoburger <juno.burger@pm.me>
2021-05-19 22:26:42 +0200

    Added missing config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,22 @@
 import Head from 'next/head';
-// import Image from 'next/image';
-import styled from 'styled-components';
 
+// import Image from 'next/image';
 import BaseLayout from '../components/layout/BaseLayout';
 import styles from '../styles/Home.module.css';
 
-
-
 export default function Home(): JSX.Element {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Starting grounds</title>
-        <meta name="description" content="Generated" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <BaseLayout>
-        <main className={styles.main}>
-          Build something cool.
-        </main>
-      </BaseLayout>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Starting grounds</title>
+                <meta name="description" content="Generated" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <BaseLayout>
+                <main className={styles.main}>Build something cool.*</main>
+            </BaseLayout>
 
-      <footer className={styles.footer}>footer</footer>
-    </div>
-  );
+            <footer className={styles.footer}>footer</footer>
+        </div>
+    );
 }
