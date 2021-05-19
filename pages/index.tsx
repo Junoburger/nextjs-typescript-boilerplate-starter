663a54afd7f6e428503c27505224651ca23e1a90
Junoburger <juno.burger@pm.me>
2021-05-19 21:48:35 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+// import Image from 'next/image';
+import styled from 'styled-components';
+
+import BaseLayout from '../components/layout/BaseLayout';
+import styles from '../styles/Home.module.css';
+
+
+
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title></title>
+        <meta name="description" content="Generated" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <BaseLayout>
+        <main className={styles.main}>
+
+        </main>
+      </BaseLayout>
+
+      <footer className={styles.footer}>footer</footer>
+    </div>
+  );
+}
